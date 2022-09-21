4b2db132e5554f3a67c105d2b6691a29bf0104b0
hayapo <keep.forward1001@gmail.com>
2022-09-21 20:07:51 +0900

    :fire: Stylesをけした

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@
 
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { config } from '../lib/theme'
+import { config } from 'lib/theme'
 
 export default class Document extends NextDocument {
   render() {
